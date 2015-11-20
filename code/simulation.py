6ae5c51b0cf7afa7f1e0997d0d2a494d4b25e715
Jackson Chen <tiger2chen@gmail.com>
2015-11-19 23:20:00 -0700

    Finish math ia draft

--- code/simulation.py
+++ code/simulation.py
@@ -2,11 +2,25 @@
 # l = list(product(range(30,50,5), range(1,10,3)))
 
 import random
-pointsIn = 0
-x = [random.random()*4 for i in xrange(1000)]
-y = [random.random()*4 for i in xrange(1000)]
-for i in range(1000):
-    if ((x[i] - 2) ** 2 + (y[i] - 2) ** 2 <= 4):
-        print [x[i], y[i]]
-        pointsIn += 1
-# print(pointsIn)
+import time
+
+points = 1000
+cumSum = 0
+n = 50
+
+start = time.time()
+for j in range(1000):
+    pointsOut = 0
+    pointsIn = 0
+    x = [random.random()*4 for i in xrange(n)]
+    y = [random.random()*4 for i in xrange(n)]
+    for i in range(n):
+        if ((x[i] - 2) ** 2 + (y[i] - 2) ** 2 <= 4):
+            pointsIn += 1
+    cumSum += pointsIn
+
+f = open('result.txt', 'w')
+f.write(str(cumSum/1000))
+f.close()
+end = time.time()
+print end - start
